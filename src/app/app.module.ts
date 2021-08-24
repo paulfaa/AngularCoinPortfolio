8e7dd8fbd1d4dbd11e43d70cfc1f4b34a9fa20e0
Paul <paulfarrell12@gmail.com>
2021-08-24 22:29:20 +0100

    Created pipe to format number values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/pipes/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicSelectableModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicSelectableModule, SharedModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
