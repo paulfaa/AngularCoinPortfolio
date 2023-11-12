7d4f8d1640881b06f754216eb6e35cb27c9beea6
paulf <paulfarrell12@gmail.com>
2023-11-12 21:55:12 +0100

    Update value display component

--- src/app/value-display/value-display.component.ts
+++ src/app/value-display/value-display.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CryptoPurchase } from '../types/cryptoPurchase.type';
 import { PurchasesService } from '../service/purchases.service';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'value-display',
@@ -16,11 +17,31 @@ export class ValueDisplayComponent implements OnInit {
   @Output() infoPopupEvent: EventEmitter<CryptoPurchase> = new EventEmitter<CryptoPurchase>();
 
   public purchasesMatchingId$: Observable<CryptoPurchase[]>;
+  public totalValue$: Observable<number>;
+  public totalProfit$: Observable<number>;
+  public profitPercentage$: Observable<number>;
+  private expenditure$: Observable<number>;
 
   constructor(private purchasesService: PurchasesService) {}
 
   ngOnInit() {
     this.purchasesMatchingId$ = this.purchasesService.getPurchasesById(this.idInput);
+    this.totalValue$ = this.purchasesMatchingId$.pipe(
+      map(purchases => {
+        return purchases.reduce((total, purchase) => total + purchase.value.currentValue, 0);
+      })
+    )
+    this.expenditure$ = this.purchasesMatchingId$.pipe(
+      map(purchases => {
+        return purchases.reduce((total, purchase) => total + purchase.purchaseDetails.price, 0);
+      })
+    )
+    this.totalProfit$ = combineLatest([this.totalValue$, this.expenditure$]).pipe(
+      map(([value1, value2]) => value1 - value2)
+    );
+    this.profitPercentage$ = combineLatest([this.totalProfit$, this.expenditure$]).pipe(
+      map(([value1, value2]) => (value1 / value2) * 100)
+    );
   }
 
   public onDeletePurchase(purchase: CryptoPurchase): void {
@@ -30,5 +51,4 @@ export class ValueDisplayComponent implements OnInit {
   public onInfoPopup(purchase: CryptoPurchase): void {
     this.infoPopupEvent.emit(purchase);
   }
-
 }
